614886a0d31d9a3d2582c4b13ab69e474b498106
Eric Staveley <eric.staveley@gmail.com>
2019-06-05 15:33:35 -0500

    my push to my repo of our team's work

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# world wine web
+# UTA Data Bootcamp Project 2 - Analysis of Wine Ratings
+
+---
+
+### Team Members: Ryan Frescas, Eric Staveley, Tony Jones, Eric Tonian
+
+---
+
+## Project Summary
+
+Continuing on Ryan/Eric T's ETL project _"Wine and Happiness"_, we dove further into the great World of Wine! Using already scraped data from WineEnthusiast, we built a visual dashboard app made up of the following: a main 'World Wine Map' using Leaflet and a choropleth plugin; an interactive d3 graph that allows for user to compare different variables (avg. price per year, avg. rating per year) per country based on user selection; and a bar chart breakdown of the top 10 types of wine (cab sauv, red blend, etc.) per country based on the same user selection.
+
+#### ETL and App Build
+
+Two main data transformations were necessary for this project:
+  - The first involved cleaning the wine csv itself to remove and modify values that would result in issues when mapping and aggregating the data. We also were able to pull the year from the title column in order to perform time-based analysis in our graphs. These changes were then converted into a sqlite file for the Flask app. 
+  - The second involved merging the wine csv with a geojson containing country boundaries and then exporting this back into a properly formatted geojson in order to correctly display wine data on the choropleth map.
+  
+The app was built with Flask, connecting our sqlite file to the database and creating routes for the main page, map, dropdown, and the two graphs. The graph routes contain all data aggregation neccessary for the corresponding js files based on user selection of the dropdown.
+
+## Resources
+
+Wine Review Dataset- 130k wine reviews with variety, location, winery, price, and description
+https://www.kaggle.com/zynicide/wine-reviews
+
+Country Boundaries GeoJSON
+https://github.com/johan/world.geo.json
+
+Choropleth Leaflet Plugin
+https://github.com/timwis/leaflet-choropleth
+
+![glug-glug](static/images/winepour.gif)
---
+++ static/js/map.js
@@ -0,0 +1,85 @@
+// Creating map object
+const bounds = L.LatLngBounds(L.LatLng(-89.999999, -179.999999), L.LatLng(89.999999, 179.999999))
+
+var myMap = L.map("mapPlot", {
+    center: [4.5, 20],
+    zoomSnap: .1,
+    zoomDelta: .5,
+    zoom: 2.5,
+    minZoom: 2.5,
+    maxZoom: 6,
+    setMaxBounds: bounds,
+    maxBoundsViscosity: 1,
+});
+
+// Adding tile layer (now with a button)
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    noWrap: false,
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+(async function buildMap() {
+    // Link to GeoJSON
+    const path = "/mapData"
+    const data = await d3.json(path);
+
+    // Create a new choropleth layer
+    const geojson = L.choropleth(data, {
+
+        // Define what  property in the features to use
+        valueProperty: "total_wines",
+
+        // Set color scale
+        colors: ['#f3c4d7', '#dda9bb', '#c78ea1', '#b17587', '#9c5b6e', '#864256', '#712a3f', '#5b0e2a'],
+
+        // Number of breaks in step range
+        steps: 8,
+
+        // q for quartile, e for equidistant, k for k-means
+        mode: "q",
+        style: {
+            // Border color
+            color: "#fff",
+            weight: 1,
+            fillOpacity: 0.8
+        },
+
+        // Binding a pop-up to each layer
+        onEachFeature: function (feature, layer) {
+            const popupMsg = `<center><h5><button id="selDataset" onclick="buttonOptionChanged(this)">${feature.properties.name}</button></h5>Number of Wines: ${feature.properties.total_wines}<br>Avg. Wine Review: ${feature.properties.avg_score}</center>`
+            layer.bindPopup(popupMsg);
+        }
+    }).addTo(myMap);
+
+    // Set up the legend
+    const legend = L.control({
+        position: "topright"
+    });
+    legend.onAdd = function () {
+        const div = L.DomUtil.create("div", "info legend");
+        const limits = geojson.options.limits;
+        const colors = geojson.options.colors;
+
+        // Add min & max
+        const legendInfo = "<center>Wines Reviewed</center>" +
+            "<div class=\"labels\">" +
+            "<div class=\"min\">" + limits[0] + "</div>" +
+            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+            "</div>";
+
+        div.innerHTML = legendInfo;
+
+        const labels = limits.map((limit, index) => {
+            return "<li style=\"background-color: " + colors[index] + "\"></li>"
+        })
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    // Adding legend to the map
+    legend.addTo(myMap);
+})()
